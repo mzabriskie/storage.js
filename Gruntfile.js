7754667a71406b62de801384c1746048a98d989a
mzabriskie <mzabriskie@gmail.com>
2013-10-09 12:04:56 -0600

    Adding npm/grunt config

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+/*global module:false*/
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.initConfig({
+        jshint: {
+            all: ['Gruntfile.js', 'src/storage.js', 'test/spec.js']
+        },
+        qunit: {
+            all: ['test/qunit.html']
+        },
+        uglify: {
+            main: {
+                files: {
+                    'dist/storage.min.js': ['src/storage.js']
+                }
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    {src: 'src/storage.js', dest: 'dist/', expand: true, flatten: true}
+                ]
+            }
+        }
+    });
+
+    grunt.registerTask('test', ['jshint', 'qunit']);
+    grunt.registerTask('default', ['test', 'publish']);
+    grunt.registerTask('publish', ['uglify', 'copy']);
+};
