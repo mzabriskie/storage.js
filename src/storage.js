69fb423e73b28d4f86547b44ee52fa0717828570
mzabriskie <mzabriskie@gmail.com>
2013-10-09 17:04:25 -0600

    Adding ability to watch value changes

--- src/storage.js
+++ src/storage.js
@@ -6,10 +6,19 @@
         CookieAdapter;
 
     // StorageAdapter (localStorage, sessionStorage)
-    StorageAdapter = function (store) { this.store = store; };
+    StorageAdapter = function (store, type) {
+        this.store = store;
+        this.type = type;
+    };
 
     StorageAdapter.prototype.set = function (key, val) {
+        var existing = this.get(key);
+
         this.store.setItem(key, val);
+
+        if (val != existing) {
+            EventService.fireEvent(this.type, key, [existing, val]);
+        }
     };
 
     StorageAdapter.prototype.get = function (key) {
@@ -17,10 +26,16 @@
     };
 
     StorageAdapter.prototype.remove = function (key) {
+        this.set(key, null);
         this.store.removeItem(key);
     };
 
     StorageAdapter.prototype.clear = function () {
+        var keys = this.keys(),
+            i = keys.length;
+        while (i--) {
+            this.remove(keys[i]);
+        }
         this.store.clear();
     };
 
@@ -34,11 +49,20 @@
         return keys;
     };
 
+    StorageAdapter.prototype.watch = function (key, callback) {
+        EventService.addEvent(this.type, key, callback);
+    };
+
+    StorageAdapter.prototype.unwatch = function (key, callback) {
+        EventService.removeEvent(this.type, key, callback);
+    };
+
     // CookieAdapter (document.cookie)
     CookieAdapter = function () {};
 
     CookieAdapter.prototype.set = function (key, val, expires, path, domain, secure) {
-        var cookie = [];
+        var cookie = [],
+            existing = this.get(key);
         cookie.push(key + '=' + encodeURIComponent(val));
 
         if (typeof expires === 'number') {
@@ -60,6 +84,10 @@
         }
 
         document.cookie = cookie.join('; ');
+
+        if (val != existing) {
+            EventService.fireEvent('cookie', key, [existing, val]);
+        }
     };
 
     CookieAdapter.prototype.get = function (key) {
@@ -68,7 +96,7 @@
     };
 
     CookieAdapter.prototype.remove = function (key) {
-        this.set(key, '', Date.now() - 86400000);
+        this.set(key, null, Date.now() - 86400000);
     };
 
     CookieAdapter.prototype.clear = function () {
@@ -96,6 +124,58 @@
         return keys;
     };
 
+    CookieAdapter.prototype.watch = function (key, callback) {
+        EventService.addEvent('cookie', key, callback);
+    };
+
+    CookieAdapter.prototype.unwatch = function (key, callback) {
+        EventService.removeEvent('cookie', key, callback);
+    };
+
+    // Pub/Sub Service
+    var EventService = {
+        registry: {},
+
+        addEvent: function (adapter, key, callback) {
+            if (typeof this.registry[adapter] === 'undefined') {
+                this.registry[adapter] = {};
+            }
+
+            if (typeof this.registry[adapter][key] === 'undefined') {
+                this.registry[adapter][key] = [];
+            }
+
+            this.registry[adapter][key].push(callback);
+        },
+
+        removeEvent: function (adapter, key, callback) {
+            if (typeof this.registry[adapter] === 'undefined' ||
+                typeof this.registry[adapter][key] === 'undefined') {
+                return;
+            }
+
+            var queue = this.registry[adapter][key],
+                i = queue.length;
+            while (i--) {
+                if (queue[i] === callback) {
+                    queue.splice(i, 1);
+                }
+            }
+        },
+
+        fireEvent: function (adapter, key, args) {
+            if (typeof this.registry[adapter] === 'undefined' ||
+                typeof this.registry[adapter][key] === 'undefined') {
+                return;
+            }
+
+            var queue = this.registry[adapter][key];
+            for (var i=0, l=queue.length; i<l; i++) {
+                queue[i].apply(null, args);
+            }
+        }
+    };
+
     Storage = {
         cookie: function () {
             if (!cookie) {
@@ -105,13 +185,13 @@
         },
         local: function () {
             if (!local) {
-                local = new StorageAdapter(localStorage);
+                local = new StorageAdapter(localStorage, 'local');
             }
             return local;
         },
         session: function () {
             if (!session) {
-                session = new StorageAdapter(sessionStorage);
+                session = new StorageAdapter(sessionStorage, 'session');
             }
             return session;
         }
--- test/spec.js
+++ test/spec.js
@@ -13,6 +13,8 @@ QUnit.test('cookie methods', function (assert) {
     assert.equal(typeof Storage.cookie().remove, 'function');
     assert.equal(typeof Storage.cookie().clear, 'function');
     assert.equal(typeof Storage.cookie().keys, 'function');
+    assert.equal(typeof Storage.cookie().watch, 'function');
+    assert.equal(typeof Storage.cookie().unwatch, 'function');
 });
 
 QUnit.test('local methods', function (assert) {
@@ -24,6 +26,8 @@ QUnit.test('local methods', function (assert) {
     assert.equal(typeof Storage.local().remove, 'function');
     assert.equal(typeof Storage.local().clear, 'function');
     assert.equal(typeof Storage.local().keys, 'function');
+    assert.equal(typeof Storage.local().watch, 'function');
+    assert.equal(typeof Storage.local().unwatch, 'function');
 });
 
 QUnit.test('session methods', function (assert) {
@@ -35,38 +39,96 @@ QUnit.test('session methods', function (assert) {
     assert.equal(typeof Storage.session().remove, 'function');
     assert.equal(typeof Storage.session().clear, 'function');
     assert.equal(typeof Storage.session().keys, 'function');
+    assert.equal(typeof Storage.session().watch, 'function');
+    assert.equal(typeof Storage.session().unwatch, 'function');
 });
 
 ['cookie', 'local', 'session'].forEach(function (type) {
+    var storage = Storage[type]();
+
     QUnit.module(type);
     QUnit.test('get/set', function (assert) {
-        Storage[type]().set('foo', 'bar');
-        assert.equal(Storage[type]().get('foo'), 'bar');
+        storage.set('foo', 'bar');
+        assert.equal(storage.get('foo'), 'bar');
     });
 
     QUnit.test('remove', function (assert) {
-        Storage[type]().set('foo', 'bar');
-        Storage[type]().remove('foo');
-        assert.equal(Storage[type]().get('foo'), null);
+        storage.set('foo', 'bar');
+        storage.remove('foo');
+        assert.equal(storage.get('foo'), null);
     });
 
     QUnit.test('clear', function (assert) {
-        Storage[type]().set('foo', 1);
-        Storage[type]().set('bar', 2);
-        Storage[type]().set('baz', 3);
-        Storage[type]().clear();
-        assert.equal(Storage[type]().keys().length, 0);
+        storage.set('foo', 1);
+        storage.set('bar', 2);
+        storage.set('baz', 3);
+        storage.clear();
+        assert.equal(storage.keys().length, 0);
     });
 
     QUnit.test('keys', function (assert) {
-        Storage[type]().set('foo', 1);
-        Storage[type]().set('bar', 2);
-        Storage[type]().set('baz', 3);
+        storage.set('foo', 1);
+        storage.set('bar', 2);
+        storage.set('baz', 3);
 
-        var keys = Storage[type]().keys();
+        var keys = storage.keys();
         assert.equal(keys.length, 3);
         assert.ok(keys.indexOf('foo') > -1);
         assert.ok(keys.indexOf('bar') > -1);
         assert.ok(keys.indexOf('baz') > -1);
+    });
+
+    QUnit.test('watch', function (assert) {
+        var data1 = {
+                oldVal: null,
+                newVal: null
+            },
+            data2 = {
+                oldVal: null,
+                newVal: null
+            },
+            callback1 = function (oldVal, newVal) {
+                data1.oldVal = oldVal;
+                data1.newVal = newVal;
+            },
+            callback2 = function (oldVal, newVal) {
+                data2.oldVal = oldVal;
+                data2.newVal = newVal;
+            };
+
+        storage.watch('foo', callback1);
+        storage.watch('foo', callback2);
+
+        storage.set('foo', 1);
+        assert.equal(data1.oldVal, null);
+        assert.equal(data1.newVal, 1);
+        assert.equal(data2.oldVal, null);
+        assert.equal(data2.newVal, 1);
+
+        storage.set('foo', 2);
+        assert.equal(data1.oldVal, 1);
+        assert.equal(data1.newVal, 2);
+        assert.equal(data2.oldVal, 1);
+        assert.equal(data2.newVal, 2);
+
+        storage.unwatch('foo', callback1);
+
+        storage.set('foo', 3);
+        assert.equal(data1.oldVal, 1);
+        assert.equal(data1.newVal, 2);
+        assert.equal(data2.oldVal, 2);
+        assert.equal(data2.newVal, 3);
+
+        storage.remove('foo');
+        assert.equal(data2.oldVal, 3);
+        assert.equal(data2.newVal, null);
+
+        storage.set('foo', 1);
+        assert.equal(data2.oldVal, null);
+        assert.equal(data2.newVal, 1);
+
+        storage.clear();
+        assert.equal(data2.oldVal, 1);
+        assert.equal(data2.newVal, null);
     });
 });
